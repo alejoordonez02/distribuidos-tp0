7adbebd79e85e1c95894409b0755b4fca86565f5
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-03-23 06:45:21 -0300

    feat(client): (server's) ack and deserialization

---
+++ client/common/ack.go
@@ -0,0 +1,5 @@
+package common
+
+type Response struct {
+	Ack bool
+}
--- client/common/serials.go
+++ client/common/serials.go
@@ -3,9 +3,14 @@ package common
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 )
 
-const TYPE_BET = 0x00
+const (
+	TYPE_BET           = 0x00
+	TYPE_RESPONSE_ACK  = 0x01
+	TYPE_RESPONSE_NACK = 0x02
+)
 
 type Serializable interface {
 	Serialize() []byte
@@ -18,6 +23,17 @@ func (b *Bet) Serialize() []byte {
 	b.Person.serializeInto(bytes)
 
 	return bytes.Bytes()
+}
+
+func Deserialize(bytes []byte) (Response, error) {
+	switch bytes[0] {
+	case TYPE_RESPONSE_ACK:
+		return Response{true}, nil
+	case TYPE_RESPONSE_NACK:
+		return Response{false}, nil
+	default:
+		return Response{}, errors.New("unknown response type")
+	}
 }
 
 func (p *Person) serializeInto(buf *bytes.Buffer) {
